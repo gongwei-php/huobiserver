bb651974341d08f31d1c493e779a85a5f783281d
root <root@VM6969BE3E08B62A8.local>
2026-03-31 12:16:13 +0000

    init

---
+++ config/autoload/redis.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of MineAdmin.
+ *
+ * @link     https://www.mineadmin.com
+ * @document https://doc.mineadmin.com
+ * @contact  root@imoi.cn
+ * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE
+ */
+return [
+    'default' => [
+        'host' => '127.0.0.1',
+	'password' => 'Ab111222..',
+        'port' => 6379,
+        'db' => 0,
+        'pool' => [
+            'min_connections' => 1,
+            'max_connections' => 10,
+            'connect_timeout' => 10.0,
+            'wait_timeout' => 3.0,
+            'heartbeat' => -1,
+            'max_idle_time' => (float) env('REDIS_MAX_IDLE_TIME', 60),
+        ],
+    ],
+];
