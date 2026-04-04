4a6294ae8584a8af0b27086d49e4750ee9efe4ce
gongwei-php <1178397506@qq.com>
2026-04-04 22:32:47 +0800

    1

--- config/autoload/middlewares.php
+++ config/autoload/middlewares.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
  * @contact  root@imoi.cn
  * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE
  */
+
 use Hyperf\Validation\Middleware\ValidationMiddleware;
 use Mine\Support\Middleware\CorsMiddleware;
 use Mine\Support\Middleware\RequestIdMiddleware;
@@ -21,7 +22,7 @@ return [
         // 多语言识别中间件
         TranslationMiddleware::class,
         // 跨域中间件，正式环境建议关闭。使用 Nginx 等代理服务器处理跨域问题。
-        CorsMiddleware::class,
+        // CorsMiddleware::class,
         // 验证器中间件,处理 formRequest 验证器
         ValidationMiddleware::class,
     ],
