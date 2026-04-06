85accdd8118bb526ea16002256ef47a7fe9f56fa
gongwei-php <1178397506@qq.com>
2026-04-06 17:57:07 +0800

    1

--- config/autoload/redis.php
+++ config/autoload/redis.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
 return [
     'default' => [
         'host' => '127.0.0.1',
-	'password' => 'Ab111222..',
+        'password' => 'Ab111222..',
         'port' => 6379,
         'db' => 0,
         'pool' => [
@@ -23,5 +23,11 @@ return [
             'heartbeat' => -1,
             'max_idle_time' => (float) env('REDIS_MAX_IDLE_TIME', 60),
         ],
+    ],
+    'jwt' => [
+        'host' => env('REDIS_HOST', '127.0.0.1'),
+        'password' => 'Ab111222..',
+        'port' => env('REDIS_PORT', 6379),
+        'db' => env('REDIS_DB', 0),
     ],
 ];
