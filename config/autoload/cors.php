68c3f84c846d80a909125961a0e021d85ca6b62b
gongwei-php <1178397506@qq.com>
2026-04-06 22:26:00 +0800

    1

--- config/autoload/cors.php
+++ config/autoload/cors.php
@@ -1,15 +1,12 @@
-<?php declare(strict_types=1); 
-return [ 
-    	'origin' => [ 
-		'http://localhost:2888', 
-		'http://45.93.29.17:2888',
-        	// 生产可写具体域名，开发用 * 
-		'*',
-    	], 
-	'allowed_methods' => ['*'], 
-	'allowed_headers' => ['*'], 
-	'exposed_headers' => ['*'], 
+<?php
+
+declare(strict_types=1);
+return [
+	'origin' => ['*',],
+	'allowed_methods' => ['*'],
+	'allowed_headers' => ['*'],
+	'exposed_headers' => ['*'],
 	'max_age' => 3600,
-    	// 如果你前端带 cookie/Authorization 设为 true
-    	'supports_credentials' => false,
+	// 如果你前端带 cookie/Authorization 设为 true
+	'supports_credentials' => false,
 ];
